c82d7c5f08f268e650cb8abae56492bedf91815a
Shadomi <koibas.vis@gmail.com>
2016-02-27 01:57:08 +0300

    JS-001 Обозначены основные наборы тестов

---
+++ test/scriptSpec.js
@@ -0,0 +1,47 @@
+describe('Test chart drawing library', function() {
+    describe('Array identifying', function() {
+        it('should be correct', function() {
+
+        });
+    });
+
+    describe('Histogram 1 drawing test', function() {
+        describe('the svg', function() {
+            it('should be created', function() {
+
+            });
+        });
+    });
+
+    describe('Histogram 2 drawing test', function() {
+        describe('the svg', function() {
+            it('should be created', function() {
+
+            });
+        });
+    });
+
+    describe('Scatter plot drawing test', function() {
+        describe('the svg', function() {
+            it('should be created', function() {
+
+            });
+        });
+    });
+
+    describe('Line chart drawing test', function() {
+        describe('the svg', function() {
+            it('should be created', function () {
+
+            });
+        });
+    });
+
+    describe('Stacked bar drawing test', function() {
+        describe('the svg', function() {
+            it('should be created', function() {
+
+            });
+        });
+    });
+});
