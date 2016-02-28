6c8beac6c65c193daef704714b7a6fb01149b696
Shadomi <koibas.vis@gmail.com>
2016-02-28 19:28:16 +0300

    JS-002 Реализованы методы для определения типа графика

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 Array.prototype.draw = function() {
-    switch(identifyArrayType(this)) {
+    switch(identifyChartType(this)) {
         case 'histogram1':
             drawHistogram1(this);
             break;
@@ -15,22 +15,35 @@ Array.prototype.draw = function() {
     }
 };
 Object.prototype.draw = function() {
-    switch(identifyArrayType(this)) {
+    switch(identifyChartType(this)) {
         case 'lineChart':
             drawLineChart(this);
             break;
     }
 };
 
-function identifyArrayType(array) {
-    var arrayType;
-    if (array instanceof Array) {
-        arrayType = 'arrayType';
+function identifyChartType(array) {
+    if (Object.prototype.toString.call(array) == '[object Array]') {
+        if (isHistogram1(array)) {
+            return 'histogram1';
+        }
+        if (isHistogram2(array)) {
+            return 'histogram2';
+        }
+        if (isScatterPlot(array)) {
+            return 'scatterPlot';
+        }
+        if (isStackedBarChart(array)) {
+            return 'stackedBarChart';
+        }
     }
-    if (array instanceof Object) {
-        arrayType = 'arrayType';
+    if (Object.prototype.toString.call(array) == '[object Object]') {
+        if (isLineChart(array)) {
+            return 'lineChart';
+        }
     }
-    return arrayType;
+
+    return 'cant resolve';
 }
 
 function drawHistogram1(array) {
@@ -57,3 +70,87 @@ function drawStackedBarChart(array) {
     var svg;
     return svg;
 }
+
+function isHistogram1(array) {
+    var isHistogram1 = true;
+    for (var i = 0; i < array.length; i++) {
+        if (!Number.isInteger(array[i])) {
+            isHistogram1 = false;
+            break;
+        }
+    }
+    return isHistogram1;
+}
+
+function isHistogram2(array) {
+    var isHistogram2 = true;
+    for (var i = 0; i < array.length; i++) {
+        if (!isLetter(array[i])) {
+            isHistogram2 = false;
+            break;
+        }
+    }
+    return isHistogram2;
+}
+
+function isScatterPlot(array) {
+    var isScatterPlot = true;
+    for (var i = 0; i < array.length; i++) {
+        if (Object.prototype.toString.call(array[i]) == '[object Object]') {
+            if (!(array[i].x && array[i].y && array[i].r)) {
+                isScatterPlot = false;
+                break;
+            }
+        } else {
+            isScatterPlot = false;
+            break;
+        }
+    }
+    return isScatterPlot;
+}
+
+function isLineChart(array) {
+    var isLineChart = true;
+    for (var key in array) {
+        if (Number.isInteger(parseInt(key)) || key == 'draw') {
+            for (var i = 0; i < array[key].length; i++) {
+                if (!Number.isInteger(array[key][i])) {
+                    isLineChart = false;
+                    break;
+                }
+            }
+        } else {
+            isLineChart = false
+            break;
+        }
+    }
+    return isLineChart;
+}
+
+function isStackedBarChart(array) {
+    var isStackedBarChart = true;
+    for (var i = 0; i < array.length; i++) {
+        if (Object.prototype.toString.call(array[i]) == '[object Array]') {
+            if (typeof array[i][0] != 'string') {
+                isStackedBarChart = false;
+                break;
+            } else {
+                for (var j = 1; j < array[i].length; j++) {
+                    if (!Number.isInteger(array[i][j])) {
+                        isStackedBarChart = false;
+                        break;
+                    }
+                }
+            }
+        } else {
+            isStackedBarChart = false;
+            break;
+        }
+    }
+    return isStackedBarChart;
+}
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+}
+
